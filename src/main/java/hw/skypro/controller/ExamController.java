b6fd651043a9394e018fb6ddb847ea5a014e0077
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-10-25 00:24:26 +0600

    upd

--- src/main/java/hw/skypro/controller/ExamController.java
+++ src/main/java/hw/skypro/controller/ExamController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/exam/java")
+@RequestMapping("/exam/get")
 public class ExamController {
 
     public final ExaminerService service;
